8634961a2b05e9696488242391e3b52314fb7893
tech-solution-heights <tech.solution.heights@gmail.com>
2023-03-20 10:19:54 +0500

    formspress integration

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,8 +17,8 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "././mail/contact_me.php",
+            $.ajax({                
+                url: "https://formspree.io/f/mzbqvlkl",
                 type: "POST",
                 data: {
                     name: name,
@@ -27,7 +27,12 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                success: function() {
+                crossDomain: true, // allow cross-domain request
+                success: function(data, textStatus, xhr) {
+                    console.log(xhr.status);
+                    if (xhr.status === 302) {
+                        console.log('got you');
+                    }
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -39,16 +44,29 @@ $(function() {
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+                },                
+                error: function(jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.status==0) {
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Your message has been sent. </strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
+
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    } else {
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    }                    
                 },
             })
         },
